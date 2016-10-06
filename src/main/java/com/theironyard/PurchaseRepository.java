00bc2d9453c068b4703ced1af4e83b1fc95562eb
joesonghamnida <joemorris2430@gmail.com>
2016-10-06 15:58:13 -0500

    basic database functionality

---
+++ src/main/java/com/theironyard/PurchaseRepository.java
@@ -0,0 +1,9 @@
+package com.theironyard;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by joe on 06/10/2016.
+ */
+public interface PurchaseRepository extends CrudRepository<Purchase, Integer> {
+}
