6e3442548f91f4da01b810699d25f8cfe7a969cb
tnraptor14 <tnraptor14@hotmail.com>
2017-03-10 19:34:30 -0600

    can sort purchase fields not customer yet

--- src/main/java/com/theironyard/PurchaseRepository.java
+++ src/main/java/com/theironyard/PurchaseRepository.java
@@ -1,7 +1,18 @@
 package com.theironyard;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PurchaseRepository extends CrudRepository<Purchase, Integer> {
+import java.util.List;
 
+public interface PurchaseRepository extends CrudRepository<Purchase, Integer> {
+    List<Purchase> findByCategory(String category);
+    @Override
+    List<Purchase> findAll();
+
+    @Query("SELECT DISTINCT category FROM Purchase")
+    List<String> findDistinctCategory();
+
+    List<Purchase> findAll(Sort sort);
 }
--- src/main/java/com/theironyard/PurchasesController.java
+++ src/main/java/com/theironyard/PurchasesController.java
@@ -1,13 +1,16 @@
 package com.theironyard;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Scanner;
 
 @Controller
@@ -24,11 +27,32 @@ public class PurchasesController {
             loadCustomers();
         }
         if (purchases.count() == 0) {
+            loadPurchases();
         }
     }
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home() {
+    public String home(Model model, String categoryLimit, String sortField, String order) {
+        List<Purchase> purchaseList;
+        if (categoryLimit != null && !categoryLimit.equals("All")) {
+            purchaseList = purchases.findByCategory(categoryLimit);
+        }
+        else if (sortField != null){
+            Sort sort;
+            if (order != null && !order.isEmpty()) {
+                sort = new Sort(Sort.Direction.DESC ,sortField);
+            } else {
+                sort = new Sort(sortField);
+                model.addAttribute("desc", ",desc");
+                model.addAttribute(sortField, sortField);
+            }
+            purchaseList = purchases.findAll(sort);
+        }
+        else {
+            purchaseList = purchases.findAll();
+        }
+        model.addAttribute("purchases", purchaseList);
+        model.addAttribute("categories", purchases.findDistinctCategory());
         return "home";
     }
 
@@ -44,5 +68,30 @@ public class PurchasesController {
             String[] customerArr = line.split(",");
             customers.save(new Customer(customerArr[NAME], customerArr[EMAIL]));
         }
+        scanner.close();
+    }
+
+    private void loadPurchases() throws FileNotFoundException {
+        final int CUSTOMER_ID = 0;
+        final int DATE = 1;
+        final int CREDIT_CARD = 2;
+        final int CVV = 3;
+        final int CATEGORY = 4;
+
+        File f = new File("purchases.csv");
+        Scanner scanner = new Scanner(f);
+        scanner.nextLine();
+
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] purchaseArr = line.split(",");
+            purchases.save(new Purchase(purchaseArr[DATE],
+                                        purchaseArr[CREDIT_CARD],
+                                        Integer.parseInt(purchaseArr[CVV]),
+                                        purchaseArr[CATEGORY],
+                                        customers.findOne(Integer.parseInt(purchaseArr[CUSTOMER_ID]))));
+
+        }
+        scanner.close();
     }
 }
